876f7cfbe69846f0c85e32fbc5573d0ffdeed0d9
kirandash <thekirankumardash@gmail.com>
2018-01-05 22:29:55 +0530

    3 Create services for geolocation and map.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Mycoffeeapp
 
+1. Project setup
 npm uninstall -g angular-cli
 
 npm cache clean
@@ -11,3 +12,11 @@ ng -v
 ng build --prod
 
 ng serve --prod (PWA features can only be seen in production, Also PWA must have https and it will not work http, exception is localhost)
+
+2. Service creation
+
+ng g s Geolocation
+
+import it and provide in app.module.ts
+
+3. 
---
+++ src/app/geolocation.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class GeolocationService {
+
+  constructor() { }
+
+  requestLocation(callback){
+  	//w3c geolocation API - All modern browsers support this API
+  	navigator.geolocation.getCurrentPosition(
+  		position => {
+  			// on success use the callback with the coordinates
+  			callback(position.coords);
+  		},
+  		error => {
+  			// on failure - force a null value to callback
+  			// At this place we can also detect the type of error viz. if it is a permission error etc.
+  			callback(null);
+  		}
+  	)
+  }
+
+  getMapLink(location:PlaceLocation){
+  	// Universal Link
+  	// <a href="http://maps.google.com/?q=Eiffel+Tower"></a>
+  	// <a href="http://maps.google.com/?q=34.44,56.44"></a> - lat and lng
+  	let query = "";
+  	// Check if location class has lat lng or address provided
+  	if(location.latitude){
+  		query = location.latitude + "," + location.longitude;
+  	} else {
+  		query = `${location.address}, ${location.city}`;
+  	}
+  	// Check if ipad or iphone device - if the string ipad or iphone or ipod is in useragent
+  	if(/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+  		return `http://maps.apple.com/?q=${query}`; // For iOS, apple maps app should open
+  	}else{
+  		return `http://maps.google.com/?q=${query}`; // For android, google maps app should open
+  	}
+  }
+}
