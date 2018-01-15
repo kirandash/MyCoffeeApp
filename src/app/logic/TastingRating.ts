ce5cb073d7bca957ec496f8c4d00b1ce4d9b2208
kirandash <thekirankumardash@gmail.com>
2018-01-15 15:33:52 +0530

    Add coffee ratings

--- src/app/logic/TastingRating.ts
+++ src/app/logic/TastingRating.ts
@@ -1,10 +1,14 @@
 export class TastingRating {
 	
-	aroma: number;
+	/*aroma: number;
 	body: number;
 	flavor: number;
 	intensity: number;
 	sweetness: number;
-	aftertaste: number;
+	aftertaste: number;*/
+
+	constructor(public aroma: number=null, public body: number=null, public flavor: number=null, public intensity: number=null, public sweetness: number=null, public aftertaste: number=null ){
+
+	}
 	
 }
